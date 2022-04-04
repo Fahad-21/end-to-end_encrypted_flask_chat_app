1701000d7f1e52f53fc28af3244bc1c6cd1addeb
Fahad Ali <alifahad370@gmail.com>
2022-04-04 18:36:56 +0100

    Rendering email & password page

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mail import Mail,Message
+from random import randint
+from flask_socketio import SocketIO, emit, send
