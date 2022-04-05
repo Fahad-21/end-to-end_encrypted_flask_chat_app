678026cfbe9c926b2f7853226d99b2f811bd18db
Fahad Ali <alifahad370@gmail.com>
2022-04-05 20:59:13 +0100

    Providing encryption key

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ bufferSize = 64 * 1024
 password = "please-use-a-long-and-random-password"
 app = Flask(__name__, template_folder='templates')
 mail = Mail(app)
+my_addr = 'verifytext7@gmail.com'
+app.config["MAIL_SERVER"] = 'smtp.gmail.com'
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USERNAME"] = my_addr
